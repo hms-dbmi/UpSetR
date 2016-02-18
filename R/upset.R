1b74d2dca2cb86a61debdfea21cb757db1cc5c38
JakeConway <jakeconway45@gmail.com>
2016-02-17 21:59:36 -0500

    make plot only if null

--- R/upset.R
+++ R/upset.R
@@ -207,7 +207,9 @@ upset <- function(data, nsets = 5, nintersects = 40, sets = NULL, set.metadata =
                              name.size, labels, ShadingData, shade.color, shade.alpha)
   Sizes <- Make_size_plot(Set_sizes, sets.bar.color, mb.ratio)
   
+  if(is.null(set.metadata) == F){
   set.metadata <- Make_set_metadata_plot(set.metadata, Set_names)
+  }
   
   Make_base_plot(Main_bar, Matrix, Sizes, labels, mb.ratio, att.x, att.y, New_data,
                  expression, att.pos, first.col, att.color, AllQueryData, attribute.plots,
