931b36af76f540ec89340cb745a283edf9c49b80
JakeConway <jakeconway45@gmail.com>
2016-10-14 18:29:51 -0400

    rename new variables

--- R/MainBar.R
+++ R/MainBar.R
@@ -61,7 +61,7 @@ Counter <- function(data, num_sets, start_col, name_of_sets, nintersections, mba
 
 ## Generate main bar plot
 Make_main_bar <- function(Main_bar_data, Q, show_num, ratios, customQ, number_angles,
-                          ebar, ylabel, ymax, scale_intersections, font_scale){
+                          ebar, ylabel, ymax, scale_intersections, text_scale){
   if(is.null(Q) == F){
     inter_data <- Q
     if(nrow(inter_data) != 0){
@@ -99,10 +99,10 @@ Make_main_bar <- function(Main_bar_data, Q, show_num, ratios, customQ, number_an
                     + xlab(NULL) + ylab(ylabel) +labs(title = NULL)
                     + theme(panel.background = element_rect(fill = "white"),
                             plot.margin = unit(c(0.5,0.5,0.19,0.5), "lines"), panel.border = element_blank(),
-                            axis.title.y = element_text(vjust = -0.8, size = 8.3*font_scale), axis.text.y = element_text(vjust=0.3,
-                                                                                                            size=7*font_scale )))
+                            axis.title.y = element_text(vjust = -0.8, size = 8.3*text_scale), axis.text.y = element_text(vjust=0.3,
+                                                                                                            size=7*text_scale )))
   if((show_num == "yes") || (show_num == "Yes")){
-    Main_bar_plot <- (Main_bar_plot + geom_text(aes_string(label = "freq"), size = 2.2*font_scale, vjust = -1,
+    Main_bar_plot <- (Main_bar_plot + geom_text(aes_string(label = "freq"), size = 2.2*text_scale, vjust = -1,
                                                 angle = number_angles, colour = Main_bar_data$color))
   }
   bInterDat <- NULL
