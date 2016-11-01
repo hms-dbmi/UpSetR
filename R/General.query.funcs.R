cc208974112fa50300d13898f00a308a538d3551
JakeConway <jakeconway45@gmail.com>
2016-11-01 13:31:58 -0400

    Self defined query labels for query legend. Updated vignette to include example. Closes #57

--- R/General.query.funcs.R
+++ R/General.query.funcs.R
@@ -1,19 +1,20 @@
 ## Seperate the queries between and paramters by built in(intersection and element), and custom functions
 ## Apply colors if not specified
-SeperateQueries <- function(queries, choice, palette){
+SeperateQueries <- function(queries, choice, palette) {
   seperated <- list()
-  for(i in 1:length(queries)){
-    if(is.null(queries[[i]]$color) == T){
+  for (i in 1:length(queries)) {
+    if (is.null(queries[[i]]$color) == T) {
       queries[[i]]$color <- palette[1]
       palette <- palette[-1]
     }
-    else if(is.null(queries[[i]]$color) == F){
+    else if (is.null(queries[[i]]$color) == F) {
       next
     }
   }
-  if(choice == 1){
-    for(i in 1:length(queries)){
-      if(identical(intersects, queries[[i]]$query) == T || identical(elements, queries[[i]]$query) == T){
+  if (choice == 1) {
+    for (i in 1:length(queries)) {
+      if (identical(intersects, queries[[i]]$query) == T ||
+          identical(elements, queries[[i]]$query) == T) {
         seperated <- c(seperated, list(queries[[i]]))
       }
       else{
@@ -21,9 +22,10 @@ SeperateQueries <- function(queries, choice, palette){
       }
     }
   }
-  else if(choice == 2){
-    for(i in 1:length(queries)){
-      if(identical(intersects, queries[[i]]$query) == F && identical(elements, queries[[i]]$query) == F){
+  else if (choice == 2) {
+    for (i in 1:length(queries)) {
+      if (identical(intersects, queries[[i]]$query) == F &&
+          identical(elements, queries[[i]]$query) == F) {
         seperated <- c(seperated, list(queries[[i]]))
       }
       else{
@@ -35,46 +37,64 @@ SeperateQueries <- function(queries, choice, palette){
 }
 
 ## Combine all attribute data generated from queries
-combineQueriesData <- function(Intersection, Elements, Custom, att_x, att_y){
-  all_data <- data.frame()
-  all_data <- rbind(Intersection, Elements, Custom)
-  if(length(all_data) == 0){
-    return(NULL)
+combineQueriesData <-
+  function(Intersection,
+           Elements,
+           Custom,
+           att_x,
+           att_y) {
+    all_data <- data.frame()
+    all_data <- rbind(Intersection, Elements, Custom)
+    if (length(all_data) == 0) {
+      return(NULL)
+    }
+    else{
+      return(all_data)
+    }
   }
-  else{
-    return(all_data)
-  }
-}
 
 ## Create legend data for queries
-GuideGenerator <- function(queries, palette){
+GuideGenerator <- function(queries, palette) {
   numbers <- c()
   colors <- c()
-  if(length(queries) == 0){
+  if (length(queries) == 0) {
     return(NULL)
   }
-  for(i in 1:length(queries)){
-    if(is.null(queries[[i]]$color) == T){
+  for (i in 1:length(queries)) {
+    if (is.null(queries[[i]]$color) == T) {
       queries[[i]]$color <- palette[1]
       palette <- palette[-1]
     }
-    else if(is.null(queries[[i]]$color) == F){
+    else if (is.null(queries[[i]]$color) == F) {
       queries[[i]]$color <- queries[[i]]$color
     }
     colors[i] <- queries[[i]]$color
-    numbers[i] <- paste("Query", as.character(i), sep = "")
+    if (is.null(queries[[i]]$query.name) == FALSE) {
+      numbers[i] <- queries[[i]]$query.name
+    }
+    else if (is.null(queries[[i]]$query.name) == TRUE) {
+      numbers[i] <- paste("Query", as.character(i), sep = "")
+    }
   }
   guide <- cbind(numbers, colors)
   return(as.data.frame(guide))
 }
 
 ## Make plot of that legend using legend data
-Make_legend <- function(legend){
+Make_legend <- function(legend) {
   colors <- as.character(legend$colors)
   labels <- as.character(legend$numbers)
   
-  leg <- legendGrob(labels =labels, pch=22,
-                    gp=gpar(col = colors, fill = colors, fontsize = 8),
-                    ncol = length(labels), hgap = unit(0, "lines"))
+  leg <- legendGrob(
+    labels = labels,
+    pch = 22,
+    gp = gpar(
+      col = colors,
+      fill = colors,
+      fontsize = 8
+    ),
+    ncol = length(labels),
+    hgap = unit(0, "lines")
+  )
   return(leg)
 }
