7d5a1b0b72d026bef2de5b095d84e17c8d928983
RichardJActon <richardjacton@gmail.com>
2018-03-02 00:51:34 +0000

    fixed set_size.show changes in merge, rolled back grid.null change - no Rnotebook output

--- R/upset.R
+++ R/upset.R
@@ -120,7 +120,7 @@ upset <- function(data, nsets = 5, nintersects = 40, sets = NULL, keep.order = F
                   decreasing = c(T, F), show.numbers = "yes", number.angles = 0, group.by = "degree",cutoff = NULL,
                   queries = NULL, query.legend = "none", shade.color = "gray88", shade.alpha = 0.25, matrix.dot.alpha =0.5,
                   empty.intersections = NULL, color.pal = 1, boxplot.summary = NULL, attribute.plots = NULL, scale.intersections = "identity",
-                  scale.sets = "identity", text.scale = 1, set_size.angles = 0 ){
+                  scale.sets = "identity", text.scale = 1, set_size.angles = 0 , set_size.show = FALSE ){
 
   startend <-FindStartEnd(data)
   first.col <- startend[1]
@@ -257,7 +257,7 @@ upset <- function(data, nsets = 5, nintersects = 40, sets = NULL, keep.order = F
                             mainbar.y.max, scale.intersections, text.scale, attribute.plots))
   Matrix <- Make_matrix_plot(Matrix_layout, Set_sizes, All_Freqs, point.size, line.size,
                              text.scale, labels, ShadingData, shade.alpha)
-  Sizes <- Make_size_plot(Set_sizes, sets.bar.color, mb.ratio, sets.x.label, scale.sets, text.scale, set_size.angles)
+  Sizes <- Make_size_plot(Set_sizes, sets.bar.color, mb.ratio, sets.x.label, scale.sets, text.scale, set_size.angles,set_size.show)
 
   # Make_base_plot(Main_bar, Matrix, Sizes, labels, mb.ratio, att.x, att.y, New_data,
   #                expression, att.pos, first.col, att.color, AllQueryData, attribute.plots,
